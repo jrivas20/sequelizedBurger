65ce1894cae336b59db7571e9db02cf39595e345
Jose Rivas <josevzla@Joses-MacBook-Pro.local>
2017-08-17 17:55:40 -0400

    h1

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plantain Burger', True);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Reinapepiada Burger', True);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Queso Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Aguacate Burger', false);
